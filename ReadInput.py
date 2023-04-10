0c5b4dd87da989cd76fb8c9fc87a3cce154c526c
Xang <88763282+LHSang6403@users.noreply.github.com>
2023-04-10 16:26:19 +0700

    add main

--- ReadInput.py
+++ ReadInput.py
@@ -1,10 +1,9 @@
-with open('/Users/lehoangsang/coding/Knapsack_Problem_AI/INPUT_1.txt', 'r') as f:
-    lines = f.readlines()
+def ReadInput(fileName):
+    with open(fileName, 'r') as f:
+        lines = f.readlines()
 
-n = int(lines[0])
-k = int(lines[1])
-arr = [[int(x) for x in line.split(',')] for line in lines[2:]]
+    n = int(lines[0])
+    k = int(lines[1])
+    arr = [[int(x) for x in line.split(',')] for line in lines[2:]]
 
-print(n)
-print(k)
-print(arr)
+    return n, k, arr
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("dgdfsg");
+import ReadInput as RI
+
+def main():
+    n, k, arr = RI.ReadInput("/Users/lehoangsang/coding/Knapsack_Problem_AI/INPUT_1.txt")
+    print(n)
+    print(k)
+    print(arr)
+
+main()
