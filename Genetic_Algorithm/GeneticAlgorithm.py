d27a2d76ba18654682de10c89c418fe2287ba671
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-04-19 01:17:47 +0700

    Init

---
+++ Genetic_Algorithm/GeneticAlgorithm.py
@@ -0,0 +1,6 @@
+
+def genetic(W, m, wt, v, c):
+    max_val = 0
+    chosen = [0 for i in range(n)]
+    return max_val, chosen
+            
---
+++ Genetic_Algorithm/main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+current_directory = os.getcwd()
+sys.path.append(current_directory)
+
+from DoFile import *
+from GeneticAlgorithm import (
+    genetic
+)
+
+import numpy as np
+import os
+
+# Read input data
+input_file = os.path.join(current_directory, "INPUT_3.txt")
+knapsack_capacity, num_classes, weights, values, class_labels = read_input(
+    input_file)
+
+# Set algorithm parameters
+beam_width = 100
+max_iter = 1000
+
+# Solve knapsack problem using local beam search
+best_value, best_solution = genetic(
+    knapsack_capacity, num_classes, weights, values, class_labels)
+
+# Write output data
+output_file = os.path.join(current_directory , "Genetic_Algorithm", "OUTPUT_3.txt")
+write_output(output_file, best_value, best_solution)
+
+print("\nGenetic algorithm finished")
