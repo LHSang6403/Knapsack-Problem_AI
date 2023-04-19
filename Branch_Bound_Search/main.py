12394e2a354f6fc9450470badf48c49a4f66bb54
Xang <88763282+LHSang6403@users.noreply.github.com>
2023-04-19 15:48:48 +0700

    bnb

--- Branch_Bound_Search/main.py
+++ Branch_Bound_Search/main.py
@@ -1,9 +1,13 @@
 import ReadInput as RI
+import os
+import BranchBoundSearching
 
 def main():
-    n, k, arr = RI.ReadInput("/Users/lehoangsang/coding/Knapsack_Problem_AI/INPUT_1.txt")
-    print(n)
-    print(k)
-    print(arr)
+    current_dir = os.getcwd()
+    input_file = os.path.join(current_dir, 'INPUT_1.txt')
+    #print(input_file)
+    n, k, arr = RI.ReadInput(input_file)
+
+    print(BranchBoundSearching.branch_and_bound(n, arr[0], arr[1]))
 
 main()
