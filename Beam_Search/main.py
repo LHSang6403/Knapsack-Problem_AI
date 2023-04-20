c5c042275509ad2836173ddfb9a94a81b96cafae
HuyBean <nqhuy0711@gmail.com>
2023-04-20 21:49:43 +0700

    commit

--- Beam_Search/main.py
+++ Beam_Search/main.py
@@ -1,8 +1,4 @@
 import numpy as np
-from Beam_Search import (
-    local_beam_search
-)
-from DoFile import *
 import os
 import sys
 import random
@@ -10,32 +6,30 @@ import random
 current_directory = os.getcwd()
 sys.path.append(current_directory)
 
+from DoFile import *
+
+from Beam_Search import (
+    local_beam_search
+)
+
 
 # main function
 
 
 def main():
     # Read input data
-    input_file = os.path.join(current_directory, "INPUT_1.txt")
+    input_file = os.path.join(current_directory, "INPUT_10.txt")
     knapsack_capacity, num_classes, weights, values, class_labels = read_input(
         input_file)
 
+    beam_width = 1000
     # Set algorithm parameters
-    n = len(weights)
-    if n in range(0, 10):
-        beam_width = 100
-    elif n in range(11, 50):
-        beam_width = 5000
-    elif n in range(51, 100):
-        beam_width = 10000
-    elif n in range(101, 1000):
-        beam_width = 10000 * (n/100)
     # Solve knapsack problem using local beam search
     best_value, best_solution = local_beam_search(
         knapsack_capacity, num_classes, weights, values, class_labels, beam_width)
     # Write output data
     output_file = os.path.join(
-        current_directory, "Beam_Search", "OUTPUT_1.txt")
+        current_directory, "Beam_Search", "OUTPUT_10.txt")
     write_output(output_file, best_value, best_solution)
 
     print("\nBeam search finished")
