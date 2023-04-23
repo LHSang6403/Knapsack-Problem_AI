281aa26ee0fbd15ced797ca31db5faa6ebb22dd1
Xang <88763282+LHSang6403@users.noreply.github.com>
2023-04-23 22:05:38 +0700

    bnb

--- Branch_Bound_Search/main.py
+++ Branch_Bound_Search/main.py
@@ -6,11 +6,11 @@ import HandleBNB
 def main():
     current_dir = os.getcwd()
     sys.path.append(current_dir)
-    inputFile = os.path.join(current_dir, 'INPUT_2.txt')
+    inputFile = os.path.join(current_dir, 'INPUT_7.txt')
     outputFile = os.path.join(current_dir, "Branch_Bound_Search", 
-                              "OUTPUT_2.txt")
+                              "OUTPUT_7.txt")
 
     BNB = BranchBoundSearching.Problem()
     HandleBNB.SolveKnapsackUsingBNB(BNB, inputFile, outputFile)
 
-main()
+main() 
